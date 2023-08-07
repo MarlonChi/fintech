17d4693c2a10188ce8cc96983690b07e906ba160
MarlonChi <marlonchiodelli@hotmail.com>
2023-08-07 20:29:36 -0300

    feat: css properties

--- src/Components/DateRange.tsx
+++ src/Components/DateRange.tsx
@@ -5,14 +5,14 @@ const DateRange = () => {
   const [inicio, setInicio] = useState("");
   const [final, setFinal] = useState("");
   return (
-    <form className="box" onSubmit={(e) => e.preventDefault}>
+    <form className="box flex" onSubmit={(e) => e.preventDefault}>
       <DateInput
-        label="inicio"
+        label="Inicio"
         value={inicio}
         onChange={({ target }) => setInicio(target.value)}
       />
       <DateInput
-        label="final"
+        label="Final"
         value={final}
         onChange={({ target }) => setFinal(target.value)}
       />
