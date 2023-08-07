9341e3dc73fb88099785bc83b7778bd4ff1e2624
MarlonChi <marlonchiodelli@hotmail.com>
2023-08-07 19:49:24 -0300

    feat: pages and hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import Header from "./Components/Header";
+import Sidenav from "./Components/Sidenav";
+import Resume from "./Pages/Resume";
 import "./styles/global.css";
 
 function App() {
-  return <div>Oi</div>;
+  return (
+    <div>
+      <Sidenav />
+      <main>
+        <Header />
+        <Resume />
+      </main>
+    </div>
+  );
 }
 
 export default App;
