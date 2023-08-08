fabe9087761a2dc71009157b06ef1f0c3c98b49a
MarlonChi <marlonchiodelli@hotmail.com>
2023-08-07 21:19:50 -0300

    feat: dashboard resume

--- src/Components/Months.tsx
+++ src/Components/Months.tsx
@@ -3,6 +3,7 @@ import MonthBtn from "./MonthBtn";
 const Months = () => {
   return (
     <div className="flex">
+      <MonthBtn n={-3} />
       <MonthBtn n={-2} />
       <MonthBtn n={-1} />
       <MonthBtn n={0} />
--- src/Pages/Resume.tsx
+++ src/Pages/Resume.tsx
@@ -2,8 +2,41 @@ import { useData } from "../Context/DataContext";
 
 const Resume = () => {
   const { data } = useData();
-  console.log(data);
-  return <div>Resume</div>;
+
+  if (data === null) return null;
+  return (
+    <section>
+      <div className="resumo flex mb">
+        <div className="box">
+          <h2>Vendas</h2>
+          <span>
+            {data
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+        <div className="box">
+          <h2>Recebido</h2>
+          <span>
+            {data
+              .filter((i) => i.status === "pago")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+        <div className="box">
+          <h2>Processando</h2>
+          <span>
+            {data
+              .filter((i) => i.status === "processando")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+      </div>
+      <div className="box">Gráficos</div>
+    </section>
+  );
 };
 
 export default Resume;
