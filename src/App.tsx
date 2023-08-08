6e394f2df3a25841c40742e0cb980f69fc8ca21e
MarlonChi <marlonchiodelli@hotmail.com>
2023-08-08 13:17:29 -0300

    feat: router tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { DataContextProvider } from "./Context/DataContext";
 
 import Header from "./Components/Header";
 import Sidenav from "./Components/Sidenav";
 import Resume from "./Pages/Resume";
 import Vendas from "./Pages/Vendas";
+import Venda from "./Pages/Venda";
 
 import "./styles/global.css";
 
 function App() {
   return (
-    <DataContextProvider>
-      <div className="container">
-        <Sidenav />
-        <main>
-          <Header />
-          <Resume />
-          <Vendas />
-        </main>
-      </div>
-    </DataContextProvider>
+    <BrowserRouter>
+      <DataContextProvider>
+        <div className="container">
+          <Sidenav />
+          <main>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Resume />} />
+              <Route path="/vendas" element={<Vendas />} />
+              <Route path="/vendas/:id" element={<Venda />} />
+            </Routes>
+          </main>
+        </div>
+      </DataContextProvider>
+    </BrowserRouter>
   );
 }
 
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,10 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 import DateRange from "./DateRange";
 import Months from "./Months";
 
 const Header = () => {
   const [title, setTitle] = useState("Resumo");
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      setTitle("Resumo");
+      document.title = "Fintech | Resumo";
+    } else if (location.pathname === "/vendas") {
+      setTitle("Vendas");
+      document.title = "Fintech | Vendas";
+    }
+  }, [location]);
+
   return (
     <header className="mb">
       <div className="daterange mb">
